75ad16f41cf7e96b31f897c3dc5eb605b7b5d90b
JayPatel5999 <Jpatel9102@conestogac.on.ca>
2024-01-20 16:16:55 -0500

    To-Do List App Completed

--- script.js
+++ script.js
@@ -10,6 +10,36 @@ function addTask() {
         task.innerHTML = inputBox.value;
         task.classList.add('task');
         listContainer.appendChild(task);
+        // Adds delete button to each task
+        const deleteButton = document.createElement('button');
+        deleteButton.innerHTML = '\u00d7';
+        task.appendChild(deleteButton);
         inputBox.value = '';
     }
+    saveData();
 }
+
+listContainer.addEventListener("click", function(e){
+    if(e.target.tagName === 'LI'){
+        e.target.classList.toggle('checked');
+        saveData();
+    }
+    else if(e.target.tagName === 'BUTTON'){
+        e.target.parentElement.remove();
+        saveData();
+    }
+})
+
+// Storing Data in Browser
+
+function saveData() {
+    localStorage.setItem("data", listContainer.innerHTML);
+}
+
+function loadData() {
+    const data = localStorage.getItem("data");
+    if(data){
+        listContainer.innerHTML = data;
+    }
+}
+loadData()
